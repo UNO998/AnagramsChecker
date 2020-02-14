ef5b8affe604f93c13aa4ccf6a3484a607aa6fb2
UNO998 <shenyiran2015@outlook.com>
2020-02-14 14:38:41 -0500

    Initial Server with Spring Boot

---
+++ src/test/java/com/bernie/anagrams/AnagramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernie.anagrams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
